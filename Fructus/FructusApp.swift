bb429aed19e53d4dd9900e392164409b99e3ac78
Abdelaziz Bennacer <a.bennacer@compethance.fr>
2024-05-08 10:31:43 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Abdelaziz Bennacer on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
